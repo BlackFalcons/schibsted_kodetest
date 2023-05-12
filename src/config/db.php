1b0ddc3ab1123da8467b141ecc9802e9e75ddb67
Falcon <el@naviaq.no>
2023-05-12 08:47:24 +0200

    Remove obsolete comment

--- src/config/db.php
+++ src/config/db.php
@@ -5,7 +5,7 @@ function establishDBConnection() {
         'database' => 'power_poster',
         'username' => $_SERVER['DB_USERNAME'],
         'password' => $_SERVER['DB_PASSWORD'],
-    ]; // For testing purposes only.
+    ];
 
     $dsn = 'mysql:host=' . $dbConfig['host'] . ';dbname=' . $dbConfig['database'];
     $pdo = new PDO($dsn, $dbConfig['username'], $dbConfig['password']);
